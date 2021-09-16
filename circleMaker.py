29cc9837bf3e0017f3e9d3a66ca43207ce488be6
Adam Kercheval <arkercheval@gmail.com>
2021-09-16 17:35:12 -0400

    deleting some ugly comments!

--- circleMaker.py
+++ circleMaker.py
@@ -2,7 +2,6 @@ import math
 def dist(x1, x2, y1, y2):
     radical = ((x1 - x2)**2 + (y1 - y2)**2)
     if radical > 0:
-#        print(str(math.sqrt(radical)))
         return math.sqrt(radical)
     else:
         return 0
@@ -11,7 +10,6 @@ def dist(x1, x2, y1, y2):
 def generate_good_circle():
     x = int(input("x coord of center: "))
     y = int(input("y coord of center: "))
-#    rad = int(input("radius: "))
     for rad in range(2, 20):
         print("radius: " + str(rad))
         for temp_x in range((x - rad), (x + rad + 1)):
