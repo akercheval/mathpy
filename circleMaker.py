efb0e419cd75a0892edbe2da6ac6f1fb57443374
Adam Kercheval <akercheval@users.noreply.github.com>
2021-09-17 13:07:15 -0600

    add in light comments

--- circleMaker.py
+++ circleMaker.py
@@ -10,12 +10,12 @@ def dist(x1, x2, y1, y2):
 def generate_good_circle():
     x = int(input("x coord of center: "))
     y = int(input("y coord of center: "))
-    for rad in range(2, 20):
+    for rad in range(2, 20): #arbitrary range
         print("radius: " + str(rad))
         for temp_x in range((x - rad), (x + rad + 1)):
             for temp_y in range((y - rad), (y + rad + 1)):
                 if dist(x, temp_x, y, temp_y) == rad:
-                    if x == temp_x or y == temp_y:
+                    if x == temp_x or y == temp_y: #avoid points that are on the same horizontal or vertical line as center
                         break
                     print("There's one  at (" + str(temp_x) + "," + str(temp_y) + ")")
 
