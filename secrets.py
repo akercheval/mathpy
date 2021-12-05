c40710356b3c5e77520f4ebd32c003d906ab05e4
Adam Kercheval <arkercheval@gmail.com>
2021-12-05 12:29:45 -0700

    change secrets.py to gnu gplv3

--- secrets.py
+++ secrets.py
@@ -1,13 +1,12 @@
 # Some secret recursive functions: what do they do?
 # Written by Adam Kercheval for LCRT quad text set, 12/1/21
-# For academic use, personal use, whatever: MIT license
+# For academic use, personal use, whatever: GNU GPLv3
 
 # Function 1:
 def f_one(n):
     if n == 1:
         return 1
     return (n * f_one(n - 1))
-
 
 # Function 2
 def f_two(n):
@@ -26,7 +25,6 @@ def f_three(n, i = 2): # i defaults to 2 if not provided
     if n % i == 0: # n % i returns the remainder of n / i
         return False
     return f_three(n, i + 1)
-    
 
 # Function 4
 def f_four(n):
